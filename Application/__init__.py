c63c96e560732c96f19b214f3ea8add571548cd2
Olesya Viktorova <ole72sya2012@yandex.ru>
2024-02-21 18:42:02 +0500

    first commit

---
+++ Application/__init__.py
@@ -0,0 +1,3 @@
+from application.db import *
+
+from application.salary import *
---
+++ main.py
@@ -0,0 +1,6 @@
+from application import *
+
+
+if __name__ == '__main__':
+    print(salary.calculate_salary())
+    print(people.get_employees())
